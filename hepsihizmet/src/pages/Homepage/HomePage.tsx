7ff53eab4b8edacd0e364edb9a2896f7deead97f
IlkinBairamov <ilkinabr@code.edu.az>
2022-06-03 17:17:37 +0400

    HomePage Progress

--- hepsihizmet/src/pages/Homepage/HomePage.tsx
+++ hepsihizmet/src/pages/Homepage/HomePage.tsx
@@ -113,6 +113,87 @@ export const HomePage : React.FC= ()=>{
             </div>
           </div>
         </section>
+        {/* appstore-desktop */}
+        <section className="appstore-ultraHd1">
+          <div className="container">
+          <div className="col-sm-6 download-banner-container">
+              <h4 className="widow-fix"> Telefonuna&nbsp;İndir</h4>
+              <p className="widow-fix">  Artık cebinde de hizmet pişsin, ağzına düşsün! Resmi Armut.com telefon uygulamasını&nbsp;indir.</p>
+              <div className="icon-app">
+                      <a href="https://app.adjust.com/f7wvf7e?campaign=O_Tr_Arm_iOS_C_N_U_U_Homepage_U" title="Binlerce hizmet verene ulaşmanın kolay yolu Armut'un IOS uygulamasını indirmek için App Store linki">
+                        <img src="https://cdn.armut.com/images/mobile-apps/tr:h-45/app-store-download-badge-tr.png" alt="" />
+                      </a>
+                      <a href="https://app.adjust.com/chnlj3s?campaign=O_Tr_Arm_Android_C_N_U_U_Homepage_U" title="Binlerce hizmet verene ulaşmanın kolay yolu Armut'un Android uygulamasını indirmek için Google Play Store linki">
+                        <img src="https://cdn.armut.com/images/mobile-apps/tr:h-45/play-store-download-badge-tr.png" alt="" />
+                      </a>
+                        <a href="https://app.adjust.com/qa4yd9f?campaign=O_Tr_Arm_Android_C_U_U_U_Homepage_U-Huawei" title="Binlerce hizmet verene ulaşmanın kolay yolu Armut'un Android uygulamasını indirmek için Google Play Store linki">
+                          <img src="https://cdn.armut.com/images/mobile-apps/tr:h-45/app-gallery-download-badge-tr.png" /> 
+                        </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        
+        <section className="popular" style={{marginTop:"10px"}}>
+            <div className="container text-center">
+                <div className="col-xs-12 text-armut margin-armut">
+                    Popüler Hizmetler
+                </div>
+                <div className="row">
+                    <div className="col-xs-12 text-left all-services-list">
+                        
+                                <a href="https://armut.com/bos-ev-temizligi">Boş Ev Temizliği</a>
+                            
+                                <a href="https://armut.com/boyaci-boya-badana-ustasi">Boyacı (Boya Badana Ustası)</a>
+                            
+                                <a href="https://armut.com/cam-balkon">Cam Balkon</a>
+                            
+                                <a href="https://armut.com/direksiyon-dersi">Direksiyon Dersi</a>
+                            
+                                <a href="https://armut.com/esya-depolama">Eşya Depolama</a>
+                            
+                                <a href="https://armut.com/parca-esya-tasima">Parça Eşya Taşıma</a>
+                            
+                                <a href="https://armut.com/ev-tadilat">Ev Tadilat</a>
+                            
+                                <a href="https://armut.com/ev-temizligi">Ev Temizliği</a>
+                            
+                                <a href="https://armut.com/evden-eve-nakliyat">Evden Eve Nakliyat</a>
+                            
+                                <a href="https://armut.com/fayans-doseme">Fayans Döşeme</a>
+                            
+                                <a href="https://armut.com/ic-mimar">İç Mimar</a>
+                            
+                                <a href="https://armut.com/ilkokul-ozel-ders">İlkokul Özel Ders</a>
+                            
+                                <a href="https://armut.com/koltuk-yikama-temizleme">Koltuk Yıkama Temizleme</a>
+                            
+                                <a href="https://armut.com/logo-tasarim">Logo Tasarım</a>
+                            
+                                <a href="https://armut.com/minivan-nakliye">Minivan Nakliye</a>
+                            
+                                <a href="https://armut.com/mutfak-dolabi-yapimi">Mutfak Dolabı Yapımı</a>
+                            
+                                <a href="https://armut.com/online-ingilizce-ozel-ders">Online İngilizce Özel Ders</a>
+                            
+                                <a href="https://armut.com/online-psikolog">Online Psikolog</a>
+                            
+                                <a href="https://armut.com/ortaokul-matematik-ozel-ders">Ortaokul Matematik Özel Ders</a>
+                            
+                                <a href="https://armut.com/psikolog">Psikolog</a>
+                            
+                                <a href="https://armut.com/sehirler-arasi-nakliyat">Şehirler Arası Nakliyat</a>
+                            
+                                <a href="https://armut.com/profesyonel-site-bina-ve-apartman-yonetimi">Profesyonel Site Bina ve Apartman Yönetimi</a>
+                            
+                                <a href="https://armut.com/tyt-ayt-matematik-ozel-ders">TYT AYT Matematik Özel Ders</a>
+                            
+                                <a href="https://armut.com/web-site-yapimi">Web Site Yapımı</a>
+                            
+                    </div>
+                </div>
+            </div>
+        </section>
       </>
     )
 }
